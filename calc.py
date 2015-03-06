16c0154f7e913c2eb2d24353b2ff50025faf92fb
Aleksandr Zharkov <zharkov83@gmail.com>
2015-03-06 16:45:14 +0300

    Add options page. Rewrite buttons events using Jquery.

--- calc.py
+++ calc.py
@@ -1,16 +1,36 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 from soapclient import startcalculate
+import polishrecord
+
+conf = {
+    'mode': 'SOAP',
+    'server': 'localhost',
+    'port': 8080
+}
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] ='secretverykey'
+
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('main.html', mode=conf['mode'])
 
-@app.route('/calc', methods=['POST', 'GET'] )
+@app.route('/options', methods=['GET', 'POST'])
+def options():
+    if request.method == 'POST':
+        conf['mode'] = request.form['type']
+        conf['server'] = request.form['server']
+        conf['port'] = request.form['port']
+        flash('Settings successful saved')
+    return render_template('options.html', conf=conf)
+
+@app.route('/calc', methods=['GET'])
 def calc():
     r = request.args.get('s', '2+2')
-    print r
-    return startcalculate('localhost', 8080, r)
+    if conf['mode'] == 'SOAP':
+        return startcalculate(conf['server'], int(conf['port']), r)
+    return str(polishrecord.polish(polishrecord.exptopolish(str(r))))
 
 @app.errorhandler(404)
 def page_not_found(error):
