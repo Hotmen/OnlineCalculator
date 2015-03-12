19e87d3b5bb1e60ed57466a1409200e94e48b9da
Aleksandr Zharkov <zharkov@merann.ru>
2015-03-12 09:06:08 +0300

    Delete unused files. PEP8 updates.

--- calc.py
+++ calc.py
@@ -9,12 +9,12 @@ conf = {
 }
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] ='secretverykey'
 
 
 @app.route('/')
 def index():
     return render_template('main.html', mode=conf['mode'])
+
 
 @app.route('/options', methods=['GET', 'POST'])
 def options():
@@ -25,6 +25,7 @@ def options():
         flash('Settings successful saved')
     return render_template('options.html', conf=conf)
 
+
 @app.route('/calc', methods=['GET'])
 def calc():
     r = request.args.get('s', '2+2')
@@ -32,9 +33,10 @@ def calc():
         return startcalculate(conf['server'], int(conf['port']), r)
     return str(polishrecord.polish(polishrecord.exptopolish(str(r))))
 
+
 @app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html', error = error), 404
+def page_not_found(err):
+    return render_template('404.html', error=err), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
